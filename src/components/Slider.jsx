387e091be75190dcbb4d8153100511f254346b13
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-06-11 12:31:23 +1200

    updated readme

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,69 +1,66 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth'
 import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore'
 import { db } from '../firebase.config'
-import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/bundle";
-import "swiper/css/navigation";
-import Spinner from './Spinner';
-SwiperCore.use([ Navigation, Pagination, A11y, Autoplay ])
+import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/bundle'
+import 'swiper/css/navigation'
+import Spinner from './Spinner'
+SwiperCore.use([Navigation, Pagination, A11y, Autoplay])
 
 function Slider() {
-    const auth = getAuth();
-    const [ loading, setLoading ] = useState(true);
-    const [ listings, setListings ] = useState(null);
-    const [ formData, setFormData ] = useState({
-        name: auth.currentUser
-    })
+  const auth = getAuth()
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser,
+  })
 
-    const name = formData;
+  const name = formData
 
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingsRef = collection(db, 'listings')
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
+      const querySnap = await getDocs(q)
 
-    useEffect(() => {
-          const fetchListings = async () => {
-          const listingsRef = collection(db, "listings");
-          const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
-          const querySnap = await getDocs(q);
+      let listings = []
 
-          let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
 
-          querySnap.forEach((doc) => {
-            return listings.push({
-              id: doc.id,
-              data: doc.data(),
-            });
-          });
-
-          setListings(listings);
-          setLoading(false);
-        };
-
-    fetchListings()
-    }, [])
-
-    if(loading) {
-        return <Spinner />
+      setListings(listings)
+      setLoading(false)
     }
 
-     if (listings.length === 0) {
-       return <></>;
+    fetchListings()
+  }, [])
 
-     }
+  if (loading) {
+    return <Spinner />
+  }
+
+  if (listings.length === 0) {
+    return <></>
+  }
   return (
     listings && (
       <>
-        <h1 className="text-8xl text-bolder p-20 justify-center">Kia Ora, <i>manuhiri</i> </h1>
         <Swiper
           slidesPerView={3}
           pagination={{ clickable: true }}
           navigation
           modules={[Navigation, Pagination, A11y]}
-          className="swiperSlideDiv"
+          className='swiperSlideDiv'
           loop={true}
           autoplay={{ delay: 3000 }}
           speed={1200}
@@ -74,28 +71,39 @@ function Slider() {
               onClick={() => navigate(`/category/${data.type}/${id}`)}
             >
               <div
-                className="swiperSlideDiv"
+                className='swiperSlideDiv'
                 style={{
-                  background: `url(${data.imgUrls[0]}) center no-repeat`,
-                  backgroundSize: "cover,",
+                  background: `url(${
+                    data.imgUrls[
+                      Math.floor(Math.random() * data.imgUrls.length)
+                    ]
+                  }) center no-repeat`,
+                  backgroundSize: 'cover,',
+                  height: '50vh',
+                  width: '100%',
                 }}
               >
-                <img src={id} alt="" />
-                <p className="swiperSlideText">{data.name}</p>
-                <p className="swiperSlidePrice">
-                  $
-                  {(data.discountedPrice ?? data.regularPrice)
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{' '}
-                    {data.type === 'rent' || data.type === 'bnb' ? '/ month' : null}
-                </p>
+                <div>
+                  <h1 className='swiperSlideText text-lg'>{data.name}</h1>
+                  <p className='swiperSlidePrice'>
+                    $
+                    {(data.discountedPrice ?? data.regularPrice)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
+                    {data.type === 'rent' || data.type === 'bnb'
+                      ? '/ month'
+                      : null}
+                  </p>
+                </div>
+
+                <img src={id} alt='' />
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
       </>
     )
-  );
+  )
 }
 
 export default Slider
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,81 +1,78 @@
 import { Link } from 'react-router-dom'
 import bnbImage from '../assets/houses/bnbImage.jpeg'
-import rentImage from "../assets/houses/rentImage.jpeg"
-import sellImage from "../assets/houses/sellImage.jpeg";
-import Slider from "../components/Slider"
-
-
+import rentImage from '../assets/houses/rentImage.jpeg'
+import sellImage from '../assets/houses/sellImage.jpeg'
+import Slider from '../components/Slider'
 
 function Explore() {
   return (
     <div>
       <main>
+        <div className='swiperSlideDiv col-span-3 inline-block text-center '>
+          <Slider />
+        </div>
         {/* <h1 className="hero hero-content text-8xl text-center text-bolder justify-center p-20">
          Kia Ora!
         </h1> */}
-        <div className="hero min-h-screen flex items-center">
-          <div className="flex-1 max w-4x1 mx-auto p-10">
-            <div className="grid grid-cols-3 grid-rows-15 grid-flow-row gap-5">
-              <div className="swiperSlideDiv col-span-3  inline-block pt-10 text-center p-10 ">
-                <Slider />
-              </div>
-
-              <div className="col-span-3 hero min-h-screen static">
-                <div className="hero-content flex-col lg:flex-row">
-                  <img src={rentImage} alt="rent" className="rounded-3xl" />
+        <div className='hero min-h-screen flex items-center'>
+          <div className='flex-1 max w-4x1 mx-auto p-10'>
+            <div className='grid grid-cols-3 grid-rows-15 grid-flow-row gap-5'>
+              <div className='col-span-3 hero min-h-screen static'>
+                <div className='hero-content flex-col lg:flex-row'>
+                  <img src={rentImage} alt='rent' className='rounded-3xl' />
                   <div>
-                    <h1 className="text-7xl pl-6 font-bold">Rent</h1>
-                    <p className="badge badge-accent text-lg ml-6">
+                    <h1 className='text-7xl pl-6 font-bold'>Rent</h1>
+                    <p className='badge badge-accent text-lg ml-6'>
                       Places for rent
                     </p>
-                    <p className="p-6">
+                    <p className='p-6'>
                       Provident cupiditate voluptatem et in. Quaerat fugiat ut
                       assumenda excepturi exercitationem quasi. In deleniti
                       eaque aut repudiandae et a id nisi.
                     </p>
-                    <Link to="/category/rent" className="p-6">
-                      <button className="btn btn-accent text-lg">
+                    <Link to='/category/rent' className='p-6'>
+                      <button className='btn btn-accent text-lg'>
                         Book now
                       </button>
                     </Link>
                   </div>
                 </div>
               </div>
-              <div className="col-span-3 hero min-h-screen static">
-                <div className="hero-content flex-col lg:flex-row">
-                  <img src={bnbImage} alt="bnb" className="rounded-3xl" />
+              <div className='col-span-3 hero min-h-screen static'>
+                <div className='hero-content flex-col lg:flex-row'>
+                  <img src={bnbImage} alt='bnb' className='rounded-3xl' />
                   <div>
-                    <h1 className="text-7xl pl-6 font-bold">Bed & Breakfast</h1>
-                    <p className="badge badge-success ml-6">Places to BNB</p>
-                    <p className="p-6">
+                    <h1 className='text-7xl pl-6 font-bold'>Bed & Breakfast</h1>
+                    <p className='badge badge-success ml-6'>Places to BNB</p>
+                    <p className='p-6'>
                       Provident cupiditate voluptatem et in. Quaerat fugiat ut
                       assumenda excepturi exercitationem quasi. In deleniti
                       eaque aut repudiandae et a id nisi. Lorem ipsum dolor sit
                       amet.
                     </p>
-                    <Link to="/category/bnb" className="p-6">
-                      <button className="btn btn-accent text-lg text-white">
+                    <Link to='/category/bnb' className='p-6'>
+                      <button className='btn btn-accent text-lg text-white'>
                         Book now
                       </button>
                     </Link>
                   </div>
                 </div>
               </div>
-              <div className="col-span-3 hero min-h-screen static">
-                <div className="hero-content flex-col lg:flex-row">
-                  <img src={sellImage} alt="sell" className="rounded-3xl" />
+              <div className='col-span-3 hero min-h-screen static'>
+                <div className='hero-content flex-col lg:flex-row'>
+                  <img src={sellImage} alt='sell' className='rounded-3xl' />
                   <div>
-                    <h1 className="text-7xl pl-6 font-bold">Buy</h1>
-                    <p className="badge badge-primary text-lg ml-6">
+                    <h1 className='text-7xl pl-6 font-bold'>Buy</h1>
+                    <p className='badge badge-primary text-lg ml-6'>
                       Places to Buy
                     </p>
-                    <p className="p-6">
+                    <p className='p-6'>
                       Provident cupiditate voluptatem et in. Quaerat fugiat ut
                       assumenda excepturi exercitationem quasi. In deleniti
                       eaque aut repudiandae et a id nisi.
                     </p>
-                    <Link to="/category/sale" className="p-6">
-                      <button className="btn btn-white text-lg bg-primary text-white">
+                    <Link to='/category/sale' className='p-6'>
+                      <button className='btn btn-white text-lg bg-primary text-white'>
                         Buy Now
                       </button>
                     </Link>
@@ -87,7 +84,7 @@ function Explore() {
         </div>
       </main>
     </div>
-  );
+  )
 }
 
 export default Explore
